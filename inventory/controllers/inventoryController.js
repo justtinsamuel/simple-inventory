b34987242d6b01a87c1d5d5597ef109f97b4ad44
justtinsamuel <justtinsamuel01@gmail.com>
2025-07-29 21:50:48 +0700

    Basic CRUD, express, working API, DB in MongoDB

--- inventory/controllers/inventoryController.js
+++ inventory/controllers/inventoryController.js
@@ -12,12 +12,28 @@ export const create = async (req, res) => {
 };
 
 export const update = async (req, res) => {
-  const item = await Inventory.findByIdAndUpdate(req.params.id, req.body, {
-    new: res.json(item),
-  });
+  try {
+    const item = await Inventory.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!item) return res.status(404).json({ message: "Item not found" });
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-export const remove = async (res, req) => {
-    await Inventory.findByIdAndDelete(req.params.id);
-    res.status(205).end();
-}
+export const remove = async (req, res) => {
+  try {
+    const item = await Inventory.findByIdAndDelete(req.params.id);
+
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(200).json({ message: "Item deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
